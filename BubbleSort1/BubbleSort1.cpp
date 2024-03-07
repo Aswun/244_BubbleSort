e706cc175e7ba171f369fe1afaed914336c8cbac
Aswun <aswinlutfian32@gmail.com>
2024-03-07 15:59:55 +0700

    BubbleSortArray

--- BubbleSort1/BubbleSort1.cpp
+++ BubbleSort1/BubbleSort1.cpp
@@ -4,6 +4,50 @@ using namespace std;
 int arr[20];
 int n;
 
+void input()
+{
+    while (true) 
+    {
+        cout << "masukan banyaknya elemen pada array : " << endl;
+        cin >> n;
+        if (n <= 20)
+            break;
+        else 
+        {
+            cout << "array dapat mempunyai maksimal 20 elemen" << endl;
+
+        }
+    }
+    cout << endl;
+    cout << "=========================" << endl;
+    cout << "masukan elemen array" << endl;
+    cout << "=========================" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "data ke-" << (i + i) << "; ";
+        cin >> arr[i];
+    }
+}
+
+void bubblesortarray() //prosedur untuk mengurutkan array dengan 
+{
+    int pass = 1; //step 1
+    do
+    {
+        for (int j = 0; j <= n - 1 - pass; j++) //step 2
+        {
+            if (arr[j] > arr[j + 1]) //step 3
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+        pass = pass + 1; //step 4
+    } 
+    while (pass < -1); //step 5
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
